684d8f104f0a256a656896c565bf27ee81234e83
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-06-23 17:41:23 -0300

    Botões e caixa de texto. Aprimorar com o Qt Designer.

--- tc_main.py
+++ tc_main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit
 
 class Window_Tcs (QMainWindow):
     def __init__(self):
@@ -12,13 +12,57 @@ class Window_Tcs (QMainWindow):
         self.height = 600
         self.title = "TCSystem"
 
-        # Botões
-        buttonConnect = QPushButton('Button Connect', self)
+        # Caixas de texto
+        self.insertTimes = QLineEdit(self)
+        self.insertTimes.move(700,340)
+        self.insertTimes.resize(100,50)
+
+        self.tempStart = QLineEdit(self)
+        self.tempStart.move(700, 410)
+        self.tempStart.resize(100, 50)
+
+        self.tempStep = QLineEdit(self)
+        self.tempStep.move(700, 480)
+        self.tempStep.resize(100, 50)
+
+        self.numbStep = QLineEdit(self)
+        self.numbStep.move(700, 550)
+        self.numbStep.resize(100, 50)
+
+        self.numbStep = QLineEdit(self)
+        self.numbStep.move(700, 620)
+        self.numbStep.resize(100, 50)
+
+        # BOTÕES
+        buttonConnect = QPushButton('Connect', self)
         buttonConnect.move(30,30) # .move(width_position, height_position)
         buttonConnect.resize(100,50) # .resize(width_button, height_button)
         buttonConnect.setStyleSheet('QPushButton {background-color:#800000;font:bold}')
         buttonConnect.clicked.connect(self.buttonConnectClick)
 
+        buttonMode1 = QPushButton('Mode 1', self)
+        buttonMode1.move(230, 30)  # .move(width_position, height_position)
+        buttonMode1.resize(100, 50)  # .resize(width_button, height_button)
+        buttonMode1.setStyleSheet('QPushButton')
+        buttonMode1.clicked.connect(self.buttonMode1Click)
+
+        buttonMode2 = QPushButton('Mode 2', self)
+        buttonMode2.move(350, 30)  # .move(width_position, height_position)
+        buttonMode2.resize(100, 50)  # .resize(width_button, height_button)
+        buttonMode2.setStyleSheet('QPushButton')
+        buttonMode2.clicked.connect(self.buttonMode2Click)
+
+        buttonStartPause = QPushButton('Start/Pause', self)
+        buttonStartPause.move(700, 200)  # .move(width_position, height_position)
+        buttonStartPause.resize(100, 50)  # .resize(width_button, height_button)
+        buttonStartPause.setStyleSheet('QPushButton')
+        buttonStartPause.clicked.connect(self.buttonStartPauseClick)
+
+        buttonCancel = QPushButton('Cancel', self)
+        buttonCancel.move(700, 270)  # .move(width_position, height_position)
+        buttonCancel.resize(100, 50)  # .resize(width_button, height_button)
+        buttonCancel.setStyleSheet('QPushButton')
+        buttonCancel.clicked.connect(self.buttonCancelClick)
 
         self.LoadWindow()
 
@@ -31,6 +75,21 @@ class Window_Tcs (QMainWindow):
     # Método para o botão de conexão
     def buttonConnectClick(self):
         print("Load Port of Arduino UNO!")
+
+    # Método para o botão Mode1
+    def buttonMode1Click(self):
+        print("Mode 1!")
+
+    # Método para o botão Mode1
+    def buttonMode2Click(self):
+        print("Mode 2!")
+
+    # Método para o botão Mode1
+    def buttonStartPauseClick(self):
+        print("Start/Pause!")
+
+    def buttonCancelClick(self):
+        print("Cancel")
 
 application = QApplication(sys.argv)
 j_tcs = Window_Tcs()
