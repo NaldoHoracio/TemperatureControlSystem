950a8e90d1de179ec5d632093dbc78c7469e5eb3
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-11-20 12:28:46 -0300

    Adicionando funcionalidades.

--- aux_functions.py
+++ aux_functions.py
@@ -1,7 +1,30 @@
+from PyQt5.QtWidgets import QMessageBox
 
 def is_valid_temperature(temperature):
+    ''' 
+    # This function validating the input temperature
+    
+    # keyword arguments:
+    temperature: string
+
+    # return: True or False
+    '''
     try:
         float(temperature)
     except ValueError:
         return False
     return True
+
+def is_range(temperature):
+    ''' 
+    # This function checks if the temperature is in the range
+    
+    # keyword arguments:
+    temperature: temperature
+
+    # return: True or False
+    '''
+    if temperature >= 10.0 and temperature <= 220.0:
+        return True
+    else:
+        return False
--- tcs_main.py
+++ tcs_main.py
@@ -1,6 +1,7 @@
 import sys
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import uic, QtWidgets
-from aux_functions import is_valid_temperature
+from aux_functions import is_valid_temperature, is_range
 
 
 def function_mode_one():
@@ -8,32 +9,39 @@ def function_mode_one():
     set_time_m1 = window_tcs.setTimeM1.text()
 
     if is_valid_temperature(set_temp_m1) == False:
-        set_temp_m1 = window_tcs.setTempM1.text()
-        print("Enter with a valid format!")
+        msg_error = "Enter a valid format!\nExample: 15.5"
+        #QMessageBox.setStyleSheet(self,"background-color: rgb(0,0,0)")
+        QMessageBox.critical(window_tcs, "ERRO!", msg_error)
+        #set_temp_m1 = window_tcs.setTempM1.text()
     else:
-        print("Send value to arduin!")
-        print(round(set_temp_m1, 1))
+        if is_range(set_temp_m1) == True:
+            print("Sendo to arduin")
+        else:
+            msg_wrn = "Enter a value in range: 10 °C to 220 °C"
+            #QMessageBox.setStyleSheet(self, "background-color: rgb(0,0,0)")
+            QMessageBox.critical(window_tcs, "WARNING!", msg_wrn)
 
 def function_mode_two():
     print("Mode 2 select!")
 
 def execution_mode():
-    # Verificar se está conectado
     if window_tcs.buttonM1.isChecked() == True:
         function_mode_one()
     else:
-        print("Show ballon Select mode 1")
+        msg_mode1 = "Select mode 1!"
+        QMessageBox.critical(window_tcs, "ERRO!", msg_mode1)
 
     if window_tcs.buttonM2.isChecked() == True:
         function_mode_two()
 
 
-app_tcs = QtWidgets.QApplication([])
+if __name__ == "__main__":
+    app_tcs = QtWidgets.QApplication([])
 
-window_tcs = uic.loadUi("TCSystem.ui")
+    window_tcs = uic.loadUi("TCSystem.ui")
 
-window_tcs.startPause.clicked.connect(execution_mode)  # Modo de operação
+    window_tcs.startPause.clicked.connect(execution_mode)  # Modo de operação
 
-window_tcs.show()
+    window_tcs.show()
 
-app_tcs.exec()
+    app_tcs.exec()  
