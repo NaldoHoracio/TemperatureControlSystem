b2241e0762890172ad59d029eb894183d51048ee
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2021-03-03 15:59:22 -0300

    Update funcionalidades.

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import uic, QtWidgets
 from aux_functions import is_valid_temperature, is_range
 
-def teste_print():
-    print("Hello world!")
-
 def function_mode_one():
-    set_temp_m1 = float(window_tcs.setTempM1.text())
-    set_time_m1 = window_tcs.setTimeM1.text()
+    set_temp_m1 = window_tcs.setTempM1.text()
 
     if is_valid_temperature(set_temp_m1) == False:
-        msg_error = "Enter a valid format!\nExample: 15.5"
+        msg_error = "Invalid temperature!\nEnter a valid format!\nExample: 15.5"
         # QMessageBox.setStyleSheet(self,"background-color: rgb(0,0,0)")
         QMessageBox.critical(window_tcs, "ERRO!", msg_error)
         # set_temp_m1 = window_tcs.setTempM1.text()
     else:
+        set_temp_m1 = round(float(set_temp_m1),1)
         if is_range(set_temp_m1) == True:
             print("Sendo to arduin")
+            print(set_temp_m1)
         else:
             msg_wrn = "Enter a value in range: 10 °C to 220 °C"
             #QMessageBox.setStyleSheet(self, "background-color: rgb(0,0,0)")
