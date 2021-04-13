1850ef1f62cf0d9bb6488f960d56d2ab37ccc960
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2021-04-13 16:08:57 -0300

    Update main.py. Implementando play, pause e cancel.

--- main.py
+++ main.py
@@ -38,8 +38,31 @@ class MainWindow(QMainWindow):
     
     def data_mode_one(self):
         self.set_temp_m1 = self.findChild(QtWidgets.QLineEdit, 'setTempM1')
-        self.set_temp_m1.setValidator(QDoubleValidator(0.1, 220.0, 1))
-        print("Temp1: ", self.set_temp_m1)
+        value_temp_m1 = self.set_temp_m1.setValidator(QDoubleValidator(0.1, 220.0, 1))
+        
+        # Botão Play
+        self.button_play = self.findChild(QtWidgets.QPushButton, 'buttonPlay')
+
+        if self.button_play.clicked.connect(self.button_play):
+            if value_temp_m1 == True:
+                print("Mode 1 activated! Segue o baile!")
+            else:
+                msg_err_temp_m1 = QMessageBox()
+                msg_err_temp_m1.setIcon(QMessageBox.Information)
+                msg_err_temp_m1.setText("Invalid format or \
+                    temperature outside the range or incomplete data! \
+                    Check the problem and input the values.")
+                msg_err_temp_m1.setWindowTitle("Warning")
+                msg_err_temp_m1.setStandardButtons(QMessageBox.Ok)
+                
+                return_val_set_temp_m1 = msg_err_temp_m1.exec()
+                
+                if return_val_set_temp_m1 == QMessageBox.Ok:
+                    pass
+                
+
+
+        #print("Temp1: ", self.set_temp_m1.text())
         print("Mode 1 activated!")
 
     def data_mode_two(self):
@@ -47,9 +70,14 @@ class MainWindow(QMainWindow):
         #print("Temp2: ", round(set_temp_start_aux,2))
         print("Mode 2 activated!")
 
+    def button_play(self):
+        # Envia os dados para o arduino e inicia o processo 
+        # caso os dados estejam corretos
+        print('Play button click')
+
 if __name__ == "__main__":
 
-    app_tcs = QtWidgets.QApplication(sys.argv)# Load the application
+    app_tcs = QtWidgets.QApplication(sys.argv)# Carrega a aplicação
 
     window_obj = MainWindow()
 
